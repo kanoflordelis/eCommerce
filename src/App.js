8331d5696c4b8539f0cf06cf8465e578d54deb80
Kano Flordelis Gomez <ka958376@gmail.com>
2019-08-31 14:46:23 -0700

    created project with create-react-app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import { HomePage } from './homepage.component';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<HomePage />
+			</div>
+		);
+	}
+}
+
+export default App;
